00446426cb1e3b1275b0a6342b0a393c11019f71
wjhandel <wjhandel@163.com>
2026-04-29 20:02:28 +0800

    Add pb/pb_migrations/1777265608_created_point_transactions.js

---
+++ pb/pb_migrations/1777265608_created_point_transactions.js
@@ -0,0 +1,110 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = new Collection({
+    "createRule": "",
+    "deleteRule": "",
+    "fields": [
+      {
+        "autogeneratePattern": "[a-z0-9]{15}",
+        "help": "",
+        "hidden": false,
+        "id": "text3208210256",
+        "max": 15,
+        "min": 15,
+        "name": "id",
+        "pattern": "^[a-z0-9]+$",
+        "presentable": false,
+        "primaryKey": true,
+        "required": true,
+        "system": true,
+        "type": "text"
+      },
+      {
+        "cascadeDelete": false,
+        "collectionId": "pbc_651988227",
+        "help": "",
+        "hidden": false,
+        "id": "relation582177833",
+        "maxSelect": 1,
+        "minSelect": 0,
+        "name": "child",
+        "presentable": false,
+        "required": true,
+        "system": false,
+        "type": "relation"
+      },
+      {
+        "help": "",
+        "hidden": false,
+        "id": "select2363381545",
+        "maxSelect": 1,
+        "name": "type",
+        "presentable": false,
+        "required": true,
+        "system": false,
+        "type": "select",
+        "values": [
+          "earn",
+          "spend"
+        ]
+      },
+      {
+        "help": "",
+        "hidden": false,
+        "id": "number2392944706",
+        "max": null,
+        "min": null,
+        "name": "amount",
+        "onlyInt": false,
+        "presentable": false,
+        "required": true,
+        "system": false,
+        "type": "number"
+      },
+      {
+        "autogeneratePattern": "",
+        "help": "",
+        "hidden": false,
+        "id": "text1001949196",
+        "max": 0,
+        "min": 0,
+        "name": "reason",
+        "pattern": "",
+        "presentable": false,
+        "primaryKey": false,
+        "required": true,
+        "system": false,
+        "type": "text"
+      },
+      {
+        "autogeneratePattern": "",
+        "help": "",
+        "hidden": false,
+        "id": "text200923267",
+        "max": 0,
+        "min": 0,
+        "name": "relatedId",
+        "pattern": "",
+        "presentable": false,
+        "primaryKey": false,
+        "required": false,
+        "system": false,
+        "type": "text"
+      }
+    ],
+    "id": "pbc_1980370882",
+    "indexes": [],
+    "listRule": "@request.auth.id != ''",
+    "name": "point_transactions",
+    "system": false,
+    "type": "base",
+    "updateRule": "",
+    "viewRule": "@request.auth.id != ''"
+  });
+
+  return app.save(collection);
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_1980370882");
+
+  return app.delete(collection);
+})
