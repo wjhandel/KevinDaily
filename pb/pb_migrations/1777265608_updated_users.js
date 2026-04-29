b73606b6dfef1fbdad39bbbe5aa86a0d7be2fb96
wjhandel <wjhandel@163.com>
2026-04-29 20:02:43 +0800

    Add pb/pb_migrations/1777265608_updated_users.js

---
+++ pb/pb_migrations/1777265608_updated_users.js
@@ -0,0 +1,65 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("_pb_users_auth_")
+
+  // add field
+  collection.fields.addAt(10, new Field({
+    "help": "",
+    "hidden": false,
+    "id": "select1466534506",
+    "maxSelect": 1,
+    "name": "role",
+    "presentable": false,
+    "required": true,
+    "system": false,
+    "type": "select",
+    "values": [
+      "parent",
+      "child"
+    ]
+  }))
+
+  // add field
+  collection.fields.addAt(11, new Field({
+    "autogeneratePattern": "",
+    "help": "",
+    "hidden": false,
+    "id": "text2100713124",
+    "max": 0,
+    "min": 0,
+    "name": "account",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": true,
+    "system": false,
+    "type": "text"
+  }))
+
+  // add field
+  collection.fields.addAt(12, new Field({
+    "help": "",
+    "hidden": false,
+    "id": "bool2165931080",
+    "name": "isAdmin",
+    "presentable": false,
+    "required": false,
+    "system": false,
+    "type": "bool"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("_pb_users_auth_")
+
+  // remove field
+  collection.fields.removeById("select1466534506")
+
+  // remove field
+  collection.fields.removeById("text2100713124")
+
+  // remove field
+  collection.fields.removeById("bool2165931080")
+
+  return app.save(collection)
+})
